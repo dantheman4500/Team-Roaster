3848a9392c0fe83316ba8d428c17d66c582e5ee2
dantheman4500 <103381483+dantheman4500@users.noreply.github.com>
2022-11-30 19:50:49 -0800

    Delete reset.js

--- reset.js
+++
@@ -1,7 +0,0 @@
-//! Used in `npm run reset` script 
-
-const fs = require('fs');
-if (fs.existsSync("./dist/Homepage.html")) {
-  fs.unlinkSync("./dist/Homepage.html");
-  console.log("/dist/ folder was deleted");
-}
